8edece28ccd636d9ed594aff4e853bbccedfc28a
Adrian Duyzer <adrianduyzer@gmail.com>
2020-04-13 20:11:38 -0400

    gamestart is a bit more fleshed out

--- src/screens/components/GameStart.js
+++ src/screens/components/GameStart.js
@@ -1,15 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  Button
+  Button,
+  Card
 } from 'reactstrap';
 import Player from './Player';
 
 export default function GameStart(props) {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3005/games.json')
+      .then(res => res.json())
+      .then((data) => {
+        setGames(data);
+      })
+      .catch(console.log)
+  });
+
+  const gameList = games.map((game) =>
+    <Card>
+      <div className="card-body">
+        <h5 className="card-title">{game.title}</h5>
+        <Button>Join</Button>
+      </div>
+    </Card>
+  );
+  
   return(
-    <Player currentPlayer={props.currentPlayer} />
+    <>
+      <Player currentPlayer={props.currentPlayer} />
+      <h5>Join Game</h5>
+      {gameList}
+      <br />
+      <br />
+      <Button>Start New Game</Button>
+    </>
   );
 }
